9164b1dde97827ba47ee880494958d9f282c587e
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-10-24 11:45:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,10 +13,10 @@ class Config:
     API_HASH = getenv("API_HASH", "abc123")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234:abcd")
     SESSION = getenv("SESSION", "session")
-    OWNER_ID = int(getenv("OWNER_ID", "1952053555"))
-    SUPPORT = getenv("SUPPORT", "https://t.me/solidprojects_chat")
-    CHANNEL = getenv("CHANNEL", "https://t.me/solidprojects")
-    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DoellBarr/solidmusic")
+    OWNER_ID = int(getenv("OWNER_ID", "5683104617"))
+    SUPPORT = getenv("SUPPORT", "https://t.me/DC_Kurukshethra")
+    CHANNEL = getenv("CHANNEL", "https://t.me/DC_LOGS")
+    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/sakhaavvaavaj93/dcmusicbot")
 
 
 config = Config()
